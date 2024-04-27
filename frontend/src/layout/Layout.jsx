beaabf06c7433f46ec8e481c8d1297fe1cad86aa
IsuruX98 <isurusanka98@gmail.com>
2024-04-27 17:26:07 +0530

    dashboard created

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import Router from "../router/Router";
 import Navbar from "../components/NavBar/Navbar";
 import Footer from "../components/Footer/Footer";
 
 const Layout = () => {
+  const location = useLocation();
+
+  // Check if the current path is '/admin'
+  const isAdminRoute = location.pathname === "/admin";
+
   return (
     <div>
-      <Navbar />
+      {/* Render Navbar and Footer only if it's not an admin route */}
+      {!isAdminRoute && <Navbar />}
       <Router />
-      <Footer />
+      {!isAdminRoute && <Footer />}
     </div>
   );
 };
--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import CoursePage from "../pages/CoursePage";
 import CourseDetail from "../pages/CourseDetail";
+import Dashboard from "../pages/Dashboard";
 
 const AppRouter = () => {
   return (
@@ -10,6 +11,7 @@ const AppRouter = () => {
       <Route path="/" element={<Home />} />
       <Route path="/courses" element={<CoursePage />} />
       <Route path="/details" element={<CourseDetail />} />
+      <Route path="/admin" element={<Dashboard />} />
     </Routes>
   );
 };
