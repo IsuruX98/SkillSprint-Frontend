9a819594f202a57e6ff280c016e27f5a2bca48ba
it21156960 <chamith227@gmail.com>
2024-05-15 02:51:14 +0530

    userManagementUpdated

--- frontend/src/pages/Admin/Features/UserManagement.jsx
+++ frontend/src/pages/Admin/Features/UserManagement.jsx
@@ -64,13 +64,13 @@ const UserManagement = () => {
 
   return (
     <div className="p-6 bg-gray-100 rounded-xl">
-      <div className="flex justify-between items-center mb-4">
+      <div className="flex justify-between items-center mb-4 ">
         <input
           type="text"
           placeholder="Search by name"
           value={searchTerm}
           onChange={handleSearch}
-          className="px-4 py-2 border border-gray-300 rounded-xl mr-2"
+          className="px-4 py-2 border border-gray-300 rounded-xl mr-2 w-full"
         />
         
       </div>
